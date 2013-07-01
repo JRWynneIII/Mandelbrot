a9e34ebefd4b7e4c0cd5e98cbeb0878fb8cde4f5
James R. Wynne III <wyn@uac4.ornl.gov>
2013-07-01 12:01:11 -0400

    Fractal like image created. removed flag==false condition from levelset forloop

--- DEmandel.c
+++ DEmandel.c
@@ -24,49 +24,69 @@ void main()
 	double temp=0.0;
 	double xder=0.0;
 	double yder=0.0;
-	double xorbit= 0.0;
-	double yorbit= 0.0;
+	double xorbit[maxiter+1];
+	double yorbit[maxiter+1];
 	long huge = 100000;
 	bool flag = false;
-	const long overflow = 400000;
+	const double overflow = 1e300;
 	double delta = threshold*(xmax-xmin)/(double)(nx-1);
 
 	for (iy=0; iy<ny; iy++)
 	{
-		cy = ymin+iy*(ymax-ymin)/(double)(ny-1);
+		cy = (ymin+iy*(ymax-ymin))/(double)(ny-1);
 		for (ix = 0; ix<nx; ix++)
 		{
-			iter, i = 0;
-			x,y,x2,y2,temp,xder,yder,xorbit,yorbit,dist = 0.0;
-			cx = xmin +ix*(xmax-xmin)/(double)(ny-1);
-	//		dist = MSetDist(cx,cy,maxiter);
+			iter = 0;
+			i = 0;
+			x = 0.0;
+			y = 0.0;
+			x2 = 0.0;
+			y2 = 0.0;
+			temp = 0.0;
+			xder = 0.0;
+			yder = 0.0;
+			dist = 0.0;
+			cx = (xmin +ix*(xmax-xmin))/(double)(ny-1);
 
-			for (iter = 0; iter<maxiter && (x2+y2 < huge) && flag!=true; iter++)
+			for (iter = 0; iter<maxiter && (x2+y2 < 10000.0); iter++)
 			{
-				//Begin normal mandel set point in/out process
+				//Begin normal mandel level set process
 				temp = x2-y2 +cx;
 				y = 2.0*x*y+cy;
 				x = temp;
 				x2 = x*x;
 				y2 = y*y;
+				xorbit[iter]=x;
+				xorbit[iter]=y;
+			}
+			//if the point escapes, find the distance from the set
+			if (x2+y2>huge)
+			{
+				xder, yder = 0;
+				i = 0;
+				flag = false;
 
-				if (x2+y2>huge)			//if the point escapes, find the distance from the set
+				for (i=0;i<iter && flag==false;i++)
 				{
-					temp = 2.0*(xorbit*xder-yorbit*yder)+1;
-					yder = 2.0*(yorbit*xder+xorbit*yder);
+					temp = 2.0*(xorbit[i]*xder-yorbit[i]*yder)+1;
+					yder = 2.0*(yorbit[i]*xder+xorbit[i]*yder);
 					xder = temp;
 					flag = fmax(fabs(xder), fabs(yder)) > overflow;
-					xorbit = x;
-					yorbit = y;
-					dist = log(x2+y2)*sqrt(x2+y2)/(double)(sqrt(xder*xder+yder*yder));  
-					printf("%d\n", dist);
-					break;
 				}
+				if (flag == false)
+				{
+					dist=(log(x2+y2)*sqrt(x2+y2))/(double)(sqrt(xder*xder+yder*yder)); 
+					printf("DIST:%d\n", dist);
+				}	
+
 			}
-			if (dist <= delta)
+			
+			if (dist < delta)
 				MSet[ix][iy] = 1;
 			else
 				MSet[ix][iy] = 0;
+		
+			printf("MSET:%d\n",MSet[ix][iy]);
 		}
 	}
 	calc_pixel_value(nx,ny,MSet,maxiter);
